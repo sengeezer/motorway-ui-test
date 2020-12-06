4202616f1b3913a0297bbace5be1ac986934e1a0
Fabio Albertin <albertin@gmail.com>
2020-12-06 21:45:31 +0000

    Converted ImageList to styled component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,32 @@
-import './ImageList.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import ImageCard from './ImageCard';
 
-const ImageList = ({ images }) => {
+const styles = {
+  root: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+    gridGap: '0 10px',
+    gridAutoRows: '10px',
+
+    '& img.main-image': {
+      width: '250px',
+      gridRowEnd: 'span 2'
+    }
+  }
+};
+
+const ImageList = ({ classes, images }) => {
   const imagesList = images.map(image => (
     <ImageCard image={image} key={image.id}/>
   ));
 
-  return <div className="imageList">{imagesList}</div>;
+  return <div className={classes.root}>{imagesList}</div>;
 };
 
-export default ImageList;
+ImageList.propTypes = {
+  classes: PropTypes.object.isRequired,
+  images: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ImageList);
